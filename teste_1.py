d23ba9fb760145ee73c18566b2983ac0cfff45ae
Julio <juliosouza1989@gmail.com>
2024-10-03 20:08:51 -0300

    Primeiro Commit

---
+++ teste_1.py
@@ -0,0 +1,30 @@
+#Programa para calcular IMC
+from decimal import Decimal
+
+peso = float(input('Entre com seu peso: '))
+altura = float(input('Entre com com sua altura: '))
+
+imc = peso / (altura * altura)
+
+print(f'Seu Indice de Massa Corporal é: {imc:.1f}')
+
+#condições para saber qual a situação do seu peso
+
+if imc <= 18.5:    
+    print('Você está abaixo do peso')
+
+if imc == 18.6:
+    print('Você está com o Peso Normal')
+
+elif imc <= 24.9:
+    print('Você está com o Peso Normal')
+
+elif imc == 25.0:   
+    print('Você está com Sobrepeso')
+
+elif imc <=29.9:
+    print('Você está com Sobrepeso')
+
+else:
+    print('Você está com obesidade')
+    
---
+++ teste_case.py
@@ -0,0 +1,64 @@
+
+from decimal import Decimal
+
+
+#Trecho para escolher a opção do serviço
+op = int(input( 'Entre com a opção desejada: \n 1-Impressão Digital \n 2-Impressão Plotter \n 3-Recorte \n' ))
+
+
+servico = op
+match servico:
+        case 1:
+            print('Impressão Digital')             
+            papel = int(input('Entre com a quandidade de impressão: '))          
+
+            valor_papel = papel * 8.0
+            print(f'O valor total de impressão é R$: {valor_papel:,.2f}')
+        
+        case 2:
+            print('Impressão Plotter')
+            op_p = int(input('\n 1-Adesivo \n 2-Banner \n Entre com a opção: '))
+            servico_1 = op_p
+
+            match servico_1:
+                case 1:
+                    print('Adesivo')
+                      
+                case 2:                    
+                    altura_lona = float(input('Entre com a altura do banner em metro: '))
+                    largura_lona = float(input('Entre com a largura do banner em metro: '))
+                    tamanho = altura_lona * largura_lona
+                    print(f'\n O tamanho total do banner é: {tamanho} \n')
+
+#condição pra saber se o valor cai no mínimo
+                    tamanho_m = 1.00
+                    if tamanho < tamanho_m:
+                        valor_total = 40.0
+                        print(f'Valor da lona entra no valor mínino R$: {valor_total}')
+                    
+                    else:
+                        valor_total = tamanho * 40.0
+                        print(f'O valor total do Banner R$: {valor_total:,.2f}')
+
+        case 3:
+            print('Recorte')
+            
+            op_r = int(input('\n 1-Recorte Especial \n 2-Corte Reto \n Entre com a opção:'))
+            servico_recorte = op_r
+
+            match servico_recorte:
+                case 1:
+                    print('O Valor é R$: 30.00')
+                      
+                case 2:
+                    print('O Valor é R$: 20.00')
+                    
+
+            
+            
+
+  
+
+            
+
+         
